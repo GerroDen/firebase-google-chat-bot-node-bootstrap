f78173abc71c53228d96ce90f3dcdfb44c268df6
Lars-Erik Kimmel <GerroDen@users.noreply.github.com>
2022-11-18 15:30:03 +0100

    Update Google request authenticity via KID in JWT header instead of HTTP header

--- functions/src/verification.ts
+++ functions/src/verification.ts
@@ -1,43 +1,48 @@
-import {promisify} from "util";
-import JwksClient from "jwks-rsa";
-import * as functions from "firebase-functions";
-import {Request} from "firebase-functions/lib/providers/https";
-import {GetPublicKeyOrSecret, JwtHeader, SigningKeyCallback, verify, VerifyOptions} from "jsonwebtoken";
-import {chatbotProjectId, chatIssuer, jwksUri} from "./config";
+import JwksClient from "jwks-rsa"
+import {
+    logger,
+    Request,
+} from "firebase-functions"
+import {
+    decode,
+    verify,
+} from "jsonwebtoken"
+import {
+    chatIssuer,
+    jwksUri,
+} from "./config"
+import { defineString } from "firebase-functions/params"
 
-const issuer = chatIssuer;
-const audience = chatbotProjectId;
-const bearerPrefix = "Bearer ";
+const chatbotProjectId = defineString("CHATBOT_PROJECT_ID")
+const issuer = chatIssuer
+const bearerPrefix = "Bearer "
 
-const jwksClient = JwksClient({jwksUri});
-// eslint-disable-next-line @typescript-eslint/ban-types
-const verifyPromise = promisify<string, GetPublicKeyOrSecret, VerifyOptions, object | undefined>(verify);
+const jwksClient = JwksClient({ jwksUri })
 
-// @see https://developers.google.com/chat/how-tos/bots-develop
-export async function verifyBearerToken(req: Request): Promise<boolean> {
-    const authorizationHeader = req.header("Authorization");
+/**
+ * @see https://developers.google.com/chat/how-tos/apps-develop?hl=en#verify_app_authenticity
+ */
+export async function verifyGChatBearerToken(req: Request): Promise<boolean> {
+    const authorizationHeader = req.header("Authorization")
     if (!authorizationHeader?.startsWith(bearerPrefix)) {
-        return false;
+        return false
     }
-    const token = authorizationHeader?.substring(bearerPrefix.length);
+    const token = authorizationHeader?.substring(bearerPrefix.length)
+    const decodedToken = decode(token, { complete: true })
+    if (!decodedToken?.header.kid) {
+        console.error("unable to decode KID from JWT header")
+        return false
+    }
     try {
-        await verifyPromise(token, getKey, {
-            audience,
+        const key = await jwksClient.getSigningKey(decodedToken?.header.kid)
+        const publicKey = key.getPublicKey()
+        await verify(token, publicKey, {
+            audience: chatbotProjectId.value(),
             issuer,
-        });
-        return true;
+        })
+        return true
     } catch (e) {
-        functions.logger.error(`invalid bearer token: ${e}`);
-        return false;
+        logger.error(`invalid bearer token: ${e}`)
+        return false
     }
-}
-
-function getKey(header: JwtHeader, callback: SigningKeyCallback): void {
-    jwksClient.getSigningKey(header.kid, (err, key) => {
-        if (err) {
-            callback(err);
-        }
-        const signingKey = ("publicKey" in key) ? key.publicKey : key.rsaPublicKey;
-        callback(null, signingKey);
-    });
 }
