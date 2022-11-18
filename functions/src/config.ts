9181c14d418abcf3c5b3531be915612ec80bb65f
Lars-Erik Kimmel <GerroDen@users.noreply.github.com>
2022-11-18 15:31:44 +0100

    Remove constant project ID in favor of firebase env var methods

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -4,5 +4,3 @@ export const region = "europe-west3"
 export const chatIssuer = "chat@system.gserviceaccount.com"
 // Url to obtain the public jwks for the issuer.
 export const jwksUri = `https://www.googleapis.com/service_accounts/v1/metadata/jwk/${chatIssuer}`
-// Intended audience of the token, which will be the project number of the bot.
-export const chatbotProjectId = "123456789"
