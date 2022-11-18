be5a456dba946c05b4709fc26c1a531f87e5a789
Lars-Erik Kimmel <GerroDen@users.noreply.github.com>
2022-11-18 15:29:30 +0100

    Reformat code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,18 @@
-import * as functions from "firebase-functions";
-import {region} from "./config";
-import {verifyBearerToken} from "./verification";
-import {chat_v1} from "googleapis/build/src/apis/chat/v1";
+import * as functions from "firebase-functions"
+import { region } from "./config"
+import { verifyBearerToken } from "./verification"
+import { chat_v1 } from "googleapis/build/src/apis/chat/v1"
 
 export const googleChatBot = functions.region(region).https.onRequest(async (req, res): Promise<void> => {
-    const tokenVerified = await verifyBearerToken(req);
+    const tokenVerified = await verifyBearerToken(req)
     if (!tokenVerified) {
-        res.sendStatus(401);
-        return;
+        res.sendStatus(401)
+        return
     }
-    const body = req.body as chat_v1.Schema$DeprecatedEvent;
-    functions.logger.log(JSON.stringify(body));
+    const body = req.body as chat_v1.Schema$DeprecatedEvent
+    functions.logger.log(JSON.stringify(body))
     const reply: chat_v1.Schema$Message = {
         text: "Hello from Firebase!",
-    };
-    res.send(reply);
-});
+    }
+    res.send(reply)
+})
